92b9461fbfce4abc08d11d5abeac78d59eae13cb
alice <alice@blocovermelho.org>
2026-03-13 11:28:47 -0300

    Initial mod setup
    System-Pluralkit-Id: cbveg

---
+++ src/main/java/xyz/alikindsys/microtonality/Microtonality.java
@@ -0,0 +1,10 @@
+package xyz.alikindsys.microtonality;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Microtonality implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/xyz/alikindsys/microtonality/client/MicrotonalityClient.java
@@ -0,0 +1,10 @@
+package xyz.alikindsys.microtonality.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MicrotonalityClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
