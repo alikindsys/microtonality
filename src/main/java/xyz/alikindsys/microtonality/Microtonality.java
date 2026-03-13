042f7405af7ad90d2cf749cba08dd05c2ef5dd8f
alice <alice@blocovermelho.org>
2026-03-13 12:47:31 -0300

    Remove `microstatement`
    I love you gal, but we seriously should've looked at the original mod code. The solution to "add block property" is to just add a block property, lol.
    
    System-Pluralkit-Id: cbveg

--- src/main/java/xyz/alikindsys/microtonality/Microtonality.java
+++ src/main/java/xyz/alikindsys/microtonality/Microtonality.java
@@ -1,14 +1,9 @@
 package xyz.alikindsys.microtonality;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.registry.RegistryIdRemapCallback;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
-import xyz.alikindsys.vendor.microstatement.StateRefresher;
 
 public class Microtonality implements ModInitializer {
     public static final IntegerProperty SCALE = IntegerProperty.create("scale" , 1, 24);
@@ -19,14 +14,6 @@ public class Microtonality implements ModInitializer {
     @Override
     public void onInitialize() {
         ModItems.init();
-
-        StateRefresher.INSTANCE.addBlockProperty(Blocks.NOTE_BLOCK, SCALE, 12);
-        StateRefresher.INSTANCE.addBlockProperty(Blocks.NOTE_BLOCK, OCTAVE, 0);
-
-        // This is reorderBlockStates, I think.
-        RegistryIdRemapCallback.event(BuiltInRegistries.BLOCK)
-                .register(s ->
-                        StateRefresher.INSTANCE.reorderStates(BuiltInRegistries.BLOCK, Block.BLOCK_STATE_REGISTRY, Block::getStateDefinition));
 
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES)
                 .register((ig) -> ig.accept(ModItems.TUNING_FORK));
--- src/main/java/xyz/alikindsys/vendor/microstatement/StatementBlockStateExtensions.java
+++
@@ -1,9 +0,0 @@
-package xyz.alikindsys.vendor.microstatement;
-
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
-
-public interface StatementBlockStateExtensions extends StatementStateExtensions<BlockState>
-{
-    Block statement_getBlock();
-}
