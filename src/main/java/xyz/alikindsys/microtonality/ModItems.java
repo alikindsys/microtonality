56a3bd2eb797b6b44ae870681c7dea0bba01c295
alice <alice@blocovermelho.org>
2026-03-13 11:53:07 -0300

    Add ModItems.java
    System-Pluralkit-Id: cbveg

---
+++ src/main/java/xyz/alikindsys/microtonality/ModItems.java
@@ -0,0 +1,33 @@
+package xyz.alikindsys.microtonality;
+
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+
+import java.util.function.Function;
+
+public class ModItems {
+
+    public static final Item TUNING_FORK = register("tuning_fork", Item::new, new Item.Properties().stacksTo(1));
+
+    // Calls <clinit>.
+    public static void init() {
+
+    }
+
+    public static <T extends Item> T register(String name, Function<Item.Properties, T> itemFactory, Item.Properties settings) {
+        // Create the item key.
+        ResourceKey<Item> itemKey = ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(Microtonality.MOD_ID, name));
+
+        // Create the item instance.
+        T item = itemFactory.apply(settings.setId(itemKey));
+
+        // Register the item.
+        Registry.register(BuiltInRegistries.ITEM, itemKey, item);
+
+        return item;
+    }
+}
