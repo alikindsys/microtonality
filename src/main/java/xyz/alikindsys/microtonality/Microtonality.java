1546c6861d1474673cb88c8fd418cde20ee2f291
alice <alice@blocovermelho.org>
2026-03-13 11:39:12 -0300

    Add state reordering on mod init
    System-Pluralkit-Id: cbveg

--- src/main/java/xyz/alikindsys/microtonality/Microtonality.java
+++ src/main/java/xyz/alikindsys/microtonality/Microtonality.java
@@ -1,10 +1,17 @@
 package xyz.alikindsys.microtonality;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.registry.RegistryIdRemapCallback;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.level.block.Block;
+import xyz.alikindsys.vendor.microstatement.StateRefresher;
 
 public class Microtonality implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        RegistryIdRemapCallback.event(BuiltInRegistries.BLOCK)
+                .register(s ->
+                        StateRefresher.INSTANCE.reorderStates(BuiltInRegistries.BLOCK, Block.BLOCK_STATE_REGISTRY, Block::getStateDefinition));
     }
 }
