89aa2d59734941edc9345d056e7421aaf2eb7a2c
alice <alice@blocovermelho.org>
2026-03-14 16:47:08 -0300

    Makes things a bit better.
    System-Pluralkit-Id: cbveg

--- src/main/java/xyz/alikindsys/microtonality/Microtonality.java
+++ src/main/java/xyz/alikindsys/microtonality/Microtonality.java
@@ -3,11 +3,12 @@ package xyz.alikindsys.microtonality;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
 public class Microtonality implements ModInitializer {
-    public static final IntegerProperty SCALE = IntegerProperty.create("scale" , 1, 24);
-    public static final IntegerProperty OCTAVE = IntegerProperty.create("octave", 0 ,1);
+
+    // I'm sad I can't get 31-TET... Maybe I'll make a modded block that adds this.
+    // Honestly patching in vanilla mechanics is silly.
+    public static final int MAX_SCALE = 24;
     public static final String MOD_ID = "microtonality";
 
 
--- src/main/java/xyz/alikindsys/microtonality/mixin/NoteBlockMixin.java
+++ src/main/java/xyz/alikindsys/microtonality/mixin/NoteBlockMixin.java
@@ -1,23 +1,27 @@
 package xyz.alikindsys.microtonality.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.NoteBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -25,59 +29,55 @@ import xyz.alikindsys.microtonality.Microtonality;
 import xyz.alikindsys.microtonality.ModItems;
 
 @Mixin(NoteBlock.class)
-public class NoteBlockMixin extends Block  {
+public abstract class NoteBlockMixin extends Block  {
+    @Unique
+    private static final IntegerProperty SCALE = IntegerProperty.create("scale" , 0, Microtonality.MAX_SCALE);
+    @Unique
+    private static final BooleanProperty OCTAVE = BooleanProperty.create("octave");
+
     @Shadow
     @Final
     public static IntegerProperty NOTE;
 
-    // Reintroduce the old way of injecting block properties.
-    // If I looked at the mixin my life would've been so much easier lel
-    // Fuck i think it could DFU this as well...
     @Inject(method = "createBlockStateDefinition", at = @At("TAIL"))
     public void microtonality$addProperties(StateDefinition.Builder<Block, BlockState> builder, CallbackInfo ci) {
-        builder.add(Microtonality.OCTAVE);
-        builder.add(Microtonality.SCALE);
+        builder.add(OCTAVE, SCALE);
     }
 
-    // Since we doubt that noteblocks will have changed much across these versions
-    // Let's port those mixins ~carefully~.
 
-    // Those are just MVs and injects. I like this.
-    // Ordinals could be slices. I'd have to learn how to make slices tho.
-    // I based on the parameter name of the original mixin, this seems to be the most valid target.
-    @ModifyVariable(method = "triggerEvent", at = @At("STORE"))
-    private float microtonality$adjustPitch(float f, @Local(argsOnly = true) BlockState state, @Local(ordinal = 0) int i) {
-        int scale = state.getValue(Microtonality.SCALE);
-        int note = i + 24 * state.getValue(Microtonality.OCTAVE);
+    @ModifyExpressionValue(method = "triggerEvent", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/NoteBlock;getPitchFromNote(I)F"))
+    private float microtonality$adjustPitch(float f, @Local(argsOnly = true) BlockState state) {
+        int scale = state.getValue(SCALE);
+        int idx = state.getValue(NOTE);
+
+        int note = idx + scale * (state.getValue(OCTAVE) ? 1 : 0) ;
+
         return (float)Math.pow(2.0, (double) (note - scale) / (double) scale);
     }
 
     // Maybe this is a ModifyArg?
     @ModifyVariable(method = "useWithoutItem", at = @At("STORE"), argsOnly = true)
     private BlockState microtonality$resetTuneAtMax(BlockState state) {
-        if(state.getValue(NOTE) == 0 && state.getValue(Microtonality.OCTAVE) == 1) {
+        if(state.getValue(NOTE).equals(state.getValue(SCALE))) {
             state = state
                     .setValue(NOTE, 0)
-                    .setValue(Microtonality.OCTAVE, 0);
+                    .setValue(OCTAVE, !state.getValue(OCTAVE));
         }
-        if(state.getValue(Microtonality.SCALE) > 12 && state.getValue(NOTE) == 24 && state.getValue(Microtonality.OCTAVE) == 0) {
-            state = state.setValue(Microtonality.OCTAVE, 1).setValue(NOTE, 0);
-        }
-
         return state;
     }
 
-    @Inject(method = "useWithoutItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;cycle(Lnet/minecraft/world/level/block/state/properties/Property;)Ljava/lang/Object;"), cancellable = true)
-    private void microtonality$adjustScale(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
-        if (player.getMainHandItem().is(ModItems.TUNING_FORK)) {
-            if(state.getValue(Microtonality.SCALE) != 24) {
-                state = state.cycle(Microtonality.SCALE);
-            } else state = state.setValue(Microtonality.SCALE, 1);
-        }
+    @Inject(method = "useItemOn", at = @At(value = "HEAD"), cancellable = true)
+    private void microtonality$adjustScale(ItemStack item, BlockState state, Level level, BlockPos pos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
+        if (item.is(ModItems.TUNING_FORK)) {
+            if(state.getValue(SCALE) != Microtonality.MAX_SCALE) {
+                state = state.cycle(SCALE);
+            } else state = state.setValue(SCALE, 1);
 
-        level.setBlock(pos, state, 3);
-        player.displayClientMessage(Component.translatable("item.microtonality.tuning_fork.change_tune", state.getValue(Microtonality.SCALE)), true);
-        cir.setReturnValue(InteractionResult.CONSUME);
+
+            level.setBlock(pos, state, 3);
+            player.displayClientMessage(Component.translatable("item.microtonality.tuning_fork.change_tune", state.getValue(SCALE)), true);
+            cir.setReturnValue(InteractionResult.CONSUME);
+        }
     }
 
     public NoteBlockMixin(Properties properties) {
